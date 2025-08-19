774e80fff198c11c2d295a535df5c364628cc8e1
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-08-19 15:23:39 -0300

    Semana 02 - Servidor Web con módulo http

--- semana02/index.js
+++ semana02/index.js
@@ -1,5 +1,28 @@
+const http = require('http');
 const Product = require('./Product.js');
 const modelProduct = new Product();
+const port = 3000;
+const server = http.createServer( (request, response) => {
+    const url = request.url;
+    const method = request.method; // GET || POST || PUT || DELETE
+    console.log(url, method);
+
+    if( url == '/'){
+        response.writeHead( 200, { 'Content-Type': 'text/html' } );
+        response.end("<h1>Hola desde un Servidor NODE</h1>");
+    } else if ( url == '/products'){
+        response.writeHead( 200, { 'Content-Type': 'application/json' } );
+        response.end("Lista de Productos");
+    } else {
+        response.writeHead( 401, { 'Content-Type': 'text/html' } );
+        response.end(`<h4>Not Found</h4>`);
+    }
+
+});
+
+server.listen( port, () => {
+    console.log(`Servidor Web en el puerto ${port}`);
+})
 
 /* modelProduct.addProduct({
     name:'Mate 2',
@@ -27,6 +50,7 @@ const buscarPorId = async (id) => {
 }
 
 // buscarPorId('0ed7a0f9-717c-4cbd-b8a1-66f0123bd456');
-modelProduct.deleteProductById('0ed7a0f9-717c-4cbd-b8a1-66f0123bd450').then( r =>{
+/* modelProduct.deleteProductById('0ed7a0f9-717c-4cbd-b8a1-66f0123bd450').then( r =>{
     console.log(r);
 })
+    */
