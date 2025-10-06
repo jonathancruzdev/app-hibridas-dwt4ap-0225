a2743337c2ed960447a12b61c773ece6aca23316
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-10-06 16:48:10 -0300

    Semana 09 - React: Componentes

---
+++ semana09/src/App.jsx
@@ -0,0 +1,15 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header nombre="Componentes con React" /> 
+    </>
+  )
+}
+
+export default App
---
+++ semana09/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header({nombre}) {
+  
+    return (
+      <>
+        <header>
+            <h1> { nombre }</h1>
+        </header>
+      </>
+    )
+  }
+  
+  export default Header
