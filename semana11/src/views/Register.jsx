9614e0d3ff9d0a74e33f71bbe66b98b37dd4ef39
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-10-21 15:49:40 -0300

    Semana 11 - React Router Dom

--- semana11/src/views/Register.jsx
+++ semana11/src/views/Register.jsx
@@ -1,8 +1,27 @@
+import { NavLink } from "react-router-dom"
 const Register = () => {
-
+    
     return (
         <main className='container'>
-            <h1> Register</h1>
+            <form className="register">
+                <div className="form-header">
+                    <h1> Register</h1>
+                </div>
+                <label htmlFor="name">Nombre</label>
+                <input type="text"/>
+
+                <label htmlFor="email">Email</label>
+                <input type="email"  />
+
+                <label htmlFor="password">Contraseña</label>
+                <input type="password" />
+
+                <button type="submit">Crear Cuenta</button>
+            </form>
+
+            <div className="ingresarA">
+                <NavLink to='/'> ¿Ya tenes una cuenta? Ingresa desde Aquí</NavLink>
+            </div>
         </main>
     )
 }
